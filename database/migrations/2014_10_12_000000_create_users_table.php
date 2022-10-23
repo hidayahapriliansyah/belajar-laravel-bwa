6b843d9131b5d15c0377f75e761a49c05cc26e27
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-10-23 20:16:27 +0700

    table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->nullable();
+            $table->enum('role', ['user', 'admin']);
             $table->timestamps();
         });
     }
