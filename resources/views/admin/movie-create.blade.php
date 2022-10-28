59ac7ae46c0bdcde742230b9c6901139a797c17d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-10-28 14:29:06 +0700

    movie create part 2 3

--- resources/views/admin/movie-create.blade.php
+++ resources/views/admin/movie-create.blade.php
@@ -7,7 +7,7 @@
   <div class="col-md-12">
 
     {{-- Alert Here --}}
-    @if ($errors->all())
+    @if ($errors->any())
       <div class="alert alert-danger">
         <ul>
           @foreach ($errors->all() as $error)
@@ -15,6 +15,8 @@
           @endforeach
         </ul>
       </div>
+      {{-- @dump($errors->any()) --}}
+      {{-- @dd($errors->all()) --}}
     @endif
 
     <div class="card card-primary">
@@ -28,20 +30,20 @@
         <div class="card-body">
           <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="e.g Guardian of The Galaxy" value="{{ @old('title')}}">
+            <input type="text" class="form-control" id="title" name="title" placeholder="e.g Guardian of The Galaxy" value="{{ old('title')}}">
           </div>
           <div class="form-group">
             <label for="trailer">Trailer</label>
-            <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Video url">
+            <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Video url" value="{{ old('trailer') }}">
           </div>
           <div class="form-group">
             <label for="duration">Duration</label>
-            <input type="text" class="form-control" id="duration" name="duration" placeholder="1h 39m">
+            <input type="text" class="form-control" id="duration" name="duration" placeholder="1h 39m" value="{{ old('duration') }}">
           </div>
           <div class="form-group">
             <label>Date:</label>
             <div class="input-group date" id="release-date" data-target-input="nearest">
-              <input type="text" name="release_date" class="form-control datetimepicker-input" data-target="#release-date"/>
+              <input type="text" name="release_date" class="form-control datetimepicker-input" data-target="#release-date" value="{{ old('release_date') }}"/>
               <div class="input-group-append" data-target="#release-date" data-toggle="datetimepicker">
                 <div class="input-group-text"><i class="fa fa-calendar"></i></div>
               </div>
@@ -49,11 +51,11 @@
           </div>
           <div class="form-group">
             <label for="short-about">Casts</label>
-            <input type="text" class="form-control" id="short-about" name="casts" placeholder="Jackie Chan">
+            <input type="text" class="form-control" id="short-about" name="casts" placeholder="Jackie Chan" value="{{ old('casts') }}">
           </div>
           <div class="form-group">
             <label for="short-about">Categories</label>
-            <input type="text" class="form-control" id="short-about" name="categories" placeholder="Action, Fantasy">
+            <input type="text" class="form-control" id="short-about" name="categories" placeholder="Action, Fantasy" value="{{ old('categories') }}">
           </div>
           <div class="form-group">
             <label for="small-thumbnail">Small Thumbnail</label>
@@ -65,17 +67,17 @@
           </div>
           <div class="form-group">
             <label for="short-about">Short About</label>
-            <input type="text" class="form-control" id="short-about" name="short_about" placeholder="Awesome Movie">
+            <input type="text" class="form-control" id="short-about" name="short_about" placeholder="Awesome Movie" value="{{ old('short_about') }}">
           </div>
           <div class="form-group">
             <label for="short-about">About</label>
-            <input type="text" class="form-control" id="about" name="about" placeholder="Awesome Movie">
+            <input type="text" class="form-control" id="about" name="about" placeholder="Awesome Movie" value="{{ old('about') }}">
           </div>
           <div class="form-group">
             <label>Featured</label>
             <select class="custom-select" name="featured">
-              <option value="0">No</option>
-              <option value="1">Yes</option>
+              <option value="0" {{ old('featured') === '0' ? "selected" : "" }}>No</option>
+              <option value="1" {{ old('featured') === '1' ? "selected" : "" }}>Yes</option>
             </select>
           </div>
         </div>
@@ -88,4 +90,12 @@
     </div>
   </div>
 </div>
+@endsection
+
+@section('js')
+  <script>
+    $('#release-date').datetimepicker({
+      format: 'YYY-MM-DD'
+    });
+  </script>
 @endsection
